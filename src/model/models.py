3fe9ddfd0c6db27210c9099c700d5cf1b5655c0f
hankyul <consistant1y@ajou.ac.kr>
2021-09-04 22:20:22 +0900

    chore: Add Hybrid Model
    - Finally Hybrid model is added from now on

--- src/model/models.py
+++ src/model/models.py
@@ -1,3 +1,4 @@
+from src.model.hybrid import get_hybrid
 from src.model.resnet import get_resnet
 from src.model.resnet32 import get_resnet32
 from src.model.vit import get_vit
@@ -8,7 +9,9 @@ def get_model(model_name: str, nclass: int, **kwargs):
         model = get_resnet32(model_name, nclass, **kwargs)
     elif model_name.startswith('resnet'):
         model = get_resnet(model_name, nclass, **kwargs)
-    elif model_name.startswith('vit') or model_name.startswith('r50'):
+    elif model_name.startswith('vit'):
         model = get_vit(model_name, nclass, **kwargs)
+    elif model_name.startswith('r50'):
+        model = get_hybrid(model_name, nclass, **kwargs)
 
     return model
