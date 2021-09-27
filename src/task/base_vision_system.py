4acb7673942bfb10892dde30e8f4ca98a3fa4a77
hankyul <consistant1y@ajou.ac.kr>
2021-09-27 09:47:12 +0900

    feat: Apply torchmetric
    - Apply torchmetric
    - Refactor train, valid, test step
    - Apply hyperparameters

--- src/task/base_vision_system.py
+++ src/task/base_vision_system.py
@@ -1,6 +1,7 @@
 import pytorch_lightning as pl
 from torch import nn
 from torch.optim import SGD
+from torchmetrics import MetricCollection, Accuracy
 
 from src.lr_schedulers import CosineLR
 from src.utils import accuracy
@@ -8,7 +9,7 @@ from src.utils import accuracy
 
 class BaseVisionSystem(pl.LightningModule):
     def __init__(self,
-                 model: pl.LightningModule,
+                 model: nn.Module,
                  batch_step: int,
                  max_epoch: int = 100,
                  lr: float = 3e-2,
@@ -25,52 +26,49 @@ class BaseVisionSystem(pl.LightningModule):
         :param weight_decay: optimizer weight decay
         """
         super(BaseVisionSystem, self).__init__()
-        # self.save_hyperparameters()
-        self.model = model
+        self.save_hyperparameters()
+        self.model = self.hparams['model']
         self.criterion = nn.CrossEntropyLoss()
-        self.lr = lr
-        self.batch_step = batch_step
-        self.max_epoch = max_epoch
-        self.momentum = momentum
-        self.weight_decay = weight_decay
 
-    def forward(self, x):
-        return self.model(x)
+        metrics = MetricCollection({'top@1': Accuracy(top_k=1), 'top@5': Accuracy(top_k=5)})
+        self.train_metric = metrics.clone(prefix='train/')
+        self.valid_metric = metrics.clone(prefix='valid/')
+        self.test_metric = metrics.clone(prefix='test/')
 
     def training_step(self, batch, batch_idx, optimizer_idx=None):
         x, y = batch
-        y_hat = self(x)
+        y_hat = self.model(x)
         loss = self.criterion(y_hat, y)
-        acc1, acc5 = accuracy(y_hat, y, topk=(1, 5))
-        self.log('train_loss', loss, logger=True, sync_dist=True)
-        self.log('train_acc1', acc1, logger=True, on_epoch=True, prog_bar=True, sync_dist=True)
-        self.log('train_acc5', acc5, logger=True, on_epoch=True, prog_bar=True, sync_dist=True)
+        metric = self.train_metric(y_hat, y)
+        self.log_dict({'train/loss': loss})
+        self.log_dict(metric)
         return loss
 
-    def shared_eval_step(self, batch, batch_idx):
+    def validation_step(self, batch, batch_idx, dataloader_idx=None):
         x, y = batch
-        y_hat = self(x)
+        y_hat = self.model(x)
         loss = self.criterion(y_hat, y)
-        acc1, acc5 = accuracy(y_hat, y, topk=(1, 5))
-        return loss, acc1, acc5
-
-    def validation_step(self, batch, batch_idx):
-        loss, acc1, acc5 = self.shared_eval_step(batch, batch_idx)
-        self.log_dict({'val_loss':loss, 'val_acc1':acc1, 'val_acc5':acc5}, logger=True, on_epoch=True, prog_bar=True, sync_dist=True)
+        metric = self.valid_metric(y_hat, y)
+        self.log_dict({'valid/loss': loss}, add_dataloader_idx=True)
+        self.log_dict(metric, add_dataloader_idx=True)
         return loss
 
-    def test_step(self, batch, batch_idx):
-        loss, acc1, acc5 = self.shared_eval_step(batch, batch_idx)
-        self.log_dict({'test_loss': loss, 'test_acc1': acc1, 'test_acc5': acc5}, logger=True, on_epoch=True, prog_bar=True, sync_dist=True)
+    def test_step(self, batch, batch_idx, dataloader_idx=None):
+        x, y = batch
+        y_hat = self.model(x)
+        loss = self.criterion(y_hat, y)
+        metric = self.test_metric(y_hat, y)
+        self.log_dict({'test/loss': loss}, add_dataloader_idx=True)
+        self.log_dict(metric, add_dataloader_idx=True)
         return loss
 
     def configure_optimizers(self):
         optimizer = SGD([
-            {'params': list(set(param for name, param in self.model.named_parameters() if 'fc' in name)), 'lr': self.lr},
-            {'params': list(set(param for name, param in self.model.named_parameters() if 'fc' not in name)), 'lr': self.lr * 0.1},
-        ], momentum=self.momentum, weight_decay=self.weight_decay)
+            {'params': list(set(param for name, param in self.model.named_parameters() if 'fc' in name)), 'lr': self.hparams['lr']},
+            {'params': list(set(param for name, param in self.model.named_parameters() if 'fc' not in name)), 'lr': self.hparams['lr'] * 0.1},
+        ], momentum=self.hparams['momentum'], weight_decay=self.hparams['weight_decay'])
 
-        lr_scheduler = {'scheduler': CosineLR(optimizer, niter=self.max_epoch * self.batch_step, warmup=self.batch_step), 'interval': 'step'}
+        lr_scheduler = {'scheduler': CosineLR(optimizer, niter=self.hparams['max_epoch'] * self.hparams['batch_step'], warmup=self.hparams['batch_step']), 'interval': 'step'}
         return {'optimizer': optimizer, 'scheduler': lr_scheduler}
 
 
