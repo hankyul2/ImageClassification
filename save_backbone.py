a03e7605ce5e18007bb4453e978a1dffb844c0cd
hankyul <consistant1y@ajou.ac.kr>
2021-10-25 15:44:06 +0900

    chore: Run Experiment1
    - Run experiment for resnet, mobilenet, senet, vit

--- save_backbone.py
+++ save_backbone.py
@@ -3,8 +3,8 @@ from collections import OrderedDict
 import torch
 
 
-ckpt_path = 'log/resnet32_110_cifar100/IM-113/epoch=56_step=0.00_loss=3.665.ckpt'
-state_dict_path = 'pretrained/noisy/resnet32_110_cifar100/noisy_ratio_3.pth'
+ckpt_path = 'log/resnet32_110_cifar100/IM-126/epoch=53_step=0.00_loss=4.070.ckpt'
+state_dict_path = 'pretrained/small_sample/cifar100_small_sample_20_resnet32_110.pth'
 state_dict = OrderedDict()
 for k, v in torch.load(ckpt_path, map_location='cpu')['state_dict'].items():
     print(k)
