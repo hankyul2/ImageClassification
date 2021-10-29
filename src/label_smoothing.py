74dd965ff137145425ff57db5d7012973fe0a0e2
hankyul <consistant1y@ajou.ac.kr>
2021-10-29 10:16:42 +0900

    chore: Add dim=1 in log_softmax()
    - Add dim parameter in log_softmax()

--- src/label_smoothing.py
+++ src/label_smoothing.py
@@ -14,4 +14,4 @@ class LabelSmoothing(nn.Module):
         b, c = x.shape
         label = torch.full((b, c), self.alpha / (c - 1)).to(y.device)
         label = label.scatter(1, y.unsqueeze(1), self.certainty)
-        return self.criterion(F.log_softmax(x), label)
+        return self.criterion(F.log_softmax(x, dim=1), label)
